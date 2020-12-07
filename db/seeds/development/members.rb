e191cd58331c1bd58780515e4cfcf5ceb08464d7
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-07 10:53:37 +0900

    db : シードデータの作成、挿入

---
+++ db/seeds/development/members.rb
@@ -0,0 +1,14 @@
+names = %w[Taro Jiro Hana John Mike Sophy Bill Alex Mary Tom]
+fnames = %W[\u4F50\u85E4 \u9234\u6728 \u9AD8\u6A4B \u7530\u4E2D]
+gnames = %W[\u592A\u90CE \u6B21\u90CE \u82B1\u5B50]
+0.upto(9) do |idx|
+  Member.create(
+    number: idx + 10,
+    name: names[idx],
+    full_name: "#{fnames[idx % 4]} #{gnames[idx % 3]}",
+    email: "#{names[idx]}@example.com",
+    birthday: '1981-12-01',
+    sex: [1, 1, 2][idx % 3],
+    administrator: idx.zero?
+  )
+end
