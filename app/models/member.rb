53aacbdc8936c28546bfa283e19cdd8f60a581df
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-08 12:41:55 +0900

    feat : バリデーションのエラーメッセージを変更

--- app/models/member.rb
+++ app/models/member.rb
@@ -14,7 +14,8 @@ class Member < ApplicationRecord
             presence: true,
             format: {
               with: /\A[A-Za-z][A-Za-z0-9]*\z/,
-              allow_blank: true
+              allow_blank: true,
+              message: :invalid_member_name
             },
             length: { minimum: 2, maximum: 20, allow_blank: true },
             uniqueness: { case_sensitive: false }
