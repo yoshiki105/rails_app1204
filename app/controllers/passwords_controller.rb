189da225bf950fe89019cfa358a830c146353dc9
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-10 11:27:36 +0900

    feat : パスワード変更ページを追加

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,11 @@
 class PasswordsController < ApplicationController
+  before_action :login_required
+
+  def show
+    redirect_to :account
+  end
+
   def edit
+    @member = current_member
   end
 end
