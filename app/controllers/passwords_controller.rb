29d50a0668a6ceaba0821b3f9789db50b332d7cf
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-10 11:37:16 +0900

    feat : パスワード変更機能を追加

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,4 +8,38 @@ class PasswordsController < ApplicationController
   def edit
     @member = current_member
   end
+
+  def update
+    @member = current_member
+    current_password = account_params[:current_password]
+
+    if current_password.present?
+      if @member.authenticate(current_password)
+        @member.assign_attributes(account_params)
+        if @member.save
+          redirect_to :account, notice: 'パスワードを変更しました。'
+        else
+          render 'edit'
+        end
+      else
+        @member.errors.add(:current_password, :wrong)
+        render 'edit'
+      end
+    else
+      @member.errors.add(:current_password, :empty)
+      render 'edit'
+    end
+  end
+
+  private
+
+  def account_params
+    attrs = %i[
+      current_password
+      password
+      password_confirmation
+    ]
+
+    params.require(:account).permit(attrs)
+  end
 end
