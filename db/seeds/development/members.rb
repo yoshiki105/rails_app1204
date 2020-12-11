b57e4871ad32e69bb75e7cb0423052f44bda23ec
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-11 09:49:09 +0900

    db : ページネーション機能実装のため、seedデータを増加

--- db/seeds/development/members.rb
+++ db/seeds/development/members.rb
@@ -1,6 +1,8 @@
 names = %w[Taro Jiro Hana John Mike Sophy Bill Alex Mary Tom]
 fnames = %W[\u4F50\u85E4 \u9234\u6728 \u9AD8\u6A4B \u7530\u4E2D]
 gnames = %W[\u592A\u90CE \u6B21\u90CE \u82B1\u5B50]
+password = 'password'
+
 0.upto(9) do |idx|
   Member.create(
     number: idx + 10,
@@ -10,7 +12,21 @@ gnames = %W[\u592A\u90CE \u6B21\u90CE \u82B1\u5B50]
     birthday: '1981-12-01',
     sex: [1, 1, 2][idx % 3],
     administrator: idx.zero?,
-    password: 'pass1234',
-    password_confirmation: 'pass1234'
+    password: password,
+    password_confirmation: password
+  )
+end
+
+0.upto(29) do |idx|
+  Member.create(
+    number: idx + 20,
+    name: "John#{idx + 1}",
+    full_name: "John Doe#{idx + 1}",
+    email: "John#{idx + 1}@example.com",
+    birthday: '1981-12-01',
+    sex: 1,
+    administrator: false,
+    password: password,
+    password_confirmation: password
   )
 end
