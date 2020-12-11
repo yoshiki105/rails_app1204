e5ce46627ccbee285b3f1f54f7099cd757bc04f3
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-11 11:23:54 +0900

    seed : entriesモデルのシードデータを挿入

---
+++ db/seeds/development/entries.rb
@@ -0,0 +1,18 @@
+body =
+  "複素数体であれば、任意のCM-タイプの A は、実際、数体である\n" \
+  '定義体（英語版）(field of definition)を持っている。' \
+  "自己準同型環の可能なタイプは、対合（ロサチの対合（英語版）(Rosati involution）\n" \
+  'をもつ環として既に分類されていて、CM-タイプのアーベル多様体の分類を導き出す。'
+
+%w[Taro Jiro Hana].each do |name|
+  member = Member.find_by(name: name)
+  0.upto(9) do |idx|
+    Entry.create(
+      member_id: member,
+      title: "エントリ#{idx}",
+      body: body,
+      posted_at: 10.days.ago.advance(days: idx),
+      status: %w[draft member_only public][idx % 3]
+    )
+  end
+end
