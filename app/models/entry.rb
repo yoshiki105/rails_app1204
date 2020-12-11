827c530ce7a521cd9c1544e781baeb6cc45912af
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-11 10:35:41 +0900

    model : Entryモデルにバリデーションを追加

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,3 +1,10 @@
 class Entry < ApplicationRecord
   belongs_to :member
+
+  # ステータスカラムの値を定義
+  STATUS_VALUES = %w[draft follower_only public].freeze
+
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :body, :posted_at, presence: true
+  validates :status, inclusion: { in: STATUS_VALUES }
 end
