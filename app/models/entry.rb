8bc29f8dbbadd445864e572add94a360db9a66fb
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-11 10:46:06 +0900

    model : Entryモデルにスコープを定義

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -2,9 +2,16 @@ class Entry < ApplicationRecord
   belongs_to :member
 
   # ステータスカラムの値を定義
-  STATUS_VALUES = %w[draft follower_only public].freeze
+  STATUS_VALUES = %w[draft member_only public].freeze
 
   validates :title, presence: true, length: { maximum: 200 }
   validates :body, :posted_at, presence: true
   validates :status, inclusion: { in: STATUS_VALUES }
+
+  scope :common, -> { where(status: 'public') } # 非ログイン状態でも見ることのできるエントリ
+  scope :published, -> { where('status <> ?', 'draft') } # draft以外のエントリ
+  scope :full, lambda { |member|
+                 where('member_id = ? OR status <> ?', member.id, 'draft') # 自分が見ることのできるエントリ
+               }
+  scope :readable_for, ->(member) { member ? full(member) : common }  # ログインの可否で見る事のできるエントリを判別する
 end
