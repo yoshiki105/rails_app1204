b756e5574a31ab2d2da7ba2c6ef3de05e0919605
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-11 11:31:25 +0900

    route : entriesリソースを追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,14 @@ Rails.application.routes.draw do
   root 'top#index'
   get 'about', to: 'top#about'
 
+  # 普通のリソース
   resources :members do
     get 'search', on: :collection
+    resources :entries, only: [:index]
   end
+  resources :entries
 
+  # 単数リソース
   resource :session, only: %i[create destroy]
   resource :account, only: %i[show edit update]
   resource :password, only: %i[show edit update]
