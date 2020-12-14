26e966c3f89020fc94c27a2a0bf606433bfaf412
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-14 11:25:43 +0900

    feat : エントリ画像の表示順序を変更

---
+++ app/helpers/entries_helper.rb
@@ -0,0 +1,28 @@
+module EntriesHelper
+  # 最初の画像を表示するHTMLを生成
+  def the_first_image(entry)
+    image = entry.images.order(:id)[0]
+    render_entry_image(image) if image
+  end
+
+  # 2番目以降の画像を表示するHTMLを生成
+  def other_images(entry)
+    buffer = ''.html_safe
+
+    entry.images.order(:id)[1..-1]&.each do |image|
+      buffer << render_entry_image(image)
+    end
+
+    buffer
+  end
+
+  private
+
+  def render_entry_image(image)
+    content_tag(:div) do
+      image_tag image.data.variant(resize: '530x>'),
+                alt: image.alt_text,
+                style: 'display: block; margin: 0 auto 15px'
+    end
+  end
+end
