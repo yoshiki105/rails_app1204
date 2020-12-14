bb212154e9d405e39c4be1c089a048df0e905aee
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-14 09:54:54 +0900

    feat : エントリ画像追加ページを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_12_042928) do
+ActiveRecord::Schema.define(version: 2020_12_13_033218) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -42,6 +42,15 @@ ActiveRecord::Schema.define(version: 2020_12_12_042928) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["member_id"], name: "index_entries_on_member_id"
+  end
+
+  create_table "entry_images", force: :cascade do |t|
+    t.integer "entry_id"
+    t.string "alt_text", default: "", null: false
+    t.integer "position"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["entry_id"], name: "index_entry_images_on_entry_id"
   end
 
   create_table "members", force: :cascade do |t|
