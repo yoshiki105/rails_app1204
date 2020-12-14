4e04099277d335c490f1acd10bdd05fb82411f2c
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-14 16:25:47 +0900

    Controller : Admin::Baseクラスを定義

---
+++ app/controllers/admin/base.rb
@@ -0,0 +1,9 @@
+class Admin::Base < ApplicationController
+  before_action :admin_login_required
+
+  private
+
+  def admin_login_required
+    raise Forbidden unless current_member&.administrator?
+  end
+end
