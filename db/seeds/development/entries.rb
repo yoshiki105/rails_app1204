9711df64cbc68ef977bd0d82878324b6a31af892
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2020-12-14 15:08:03 +0900

    db : シードデータにいいねを追加

--- db/seeds/development/entries.rb
+++ db/seeds/development/entries.rb
@@ -7,12 +7,19 @@ body =
 %w[Taro Jiro Hana].each do |name|
   member = Member.find_by(name: name)
   0.upto(9) do |idx|
-    Entry.create(
+    entry = Entry.create(
       member_id: member.id,
       title: "エントリ#{idx}",
       body: body,
       posted_at: 10.days.ago.advance(days: idx),
       status: %w[draft member_only public][idx % 3]
     )
+
+    next unless [7, 8].include?(idx)
+
+    %w[John Mike Sophy].each do |name2|
+      voter = Member.find_by(name: name2)
+      voter.voted_entries << entry
+    end
   end
 end
